adcedbf1d3265f0d0b9d3cf1e9c8b88e085ef0bb
BusyHand <cool908yan@yandex.ru>
2025-11-04 01:44:39 +0500

    Add for all models repositories

---
+++ app-modules/auth/src/Repositories/UserRoleRepository.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Modules\Auth\Services;
+
+use Modules\Core\Models\Course;
+
+class UserRoleRepository
+{
+    public function findAll()
+    {
+
+    }
+
+    public function findById(Course $course)
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function updatePartial()
+    {
+
+    }
+
+    public function restore(string $courseId)
+    {
+
+    }
+
+    public function deleteSoft()
+    {
+
+    }
+
+    public function deleteHard()
+    {
+
+    }
+}
