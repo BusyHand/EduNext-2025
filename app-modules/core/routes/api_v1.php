12635e1a68524f07189db913c66c22a78dac62e0
BusyHand <cool908yan@yandex.ru>
2025-11-06 23:48:53 +0500

    Lesson crud with tests

--- app-modules/core/routes/api_v1.php
+++ app-modules/core/routes/api_v1.php
@@ -32,29 +32,21 @@ Route::prefix('lessons')->group(function () {
 
 });
 
-Route::prefix('/users/courses')->group(function () {
-    Route::get('/', [UserCourseController::class, 'findAll']);
-    Route::get('/{userCourse}', [UserCourseController::class, 'findById']);
-    Route::post('/', [UserCourseController::class, 'store']);
-    Route::put('/{userCourse}', [UserCourseController::class, 'update']);
-    Route::patch('/{userCourse}', [UserCourseController::class, 'updatePartial']);
-    Route::patch('/restore/{userCourseId}', [UserCourseController::class, 'restore']);
-    Route::delete('/{userCourse}/soft', [UserCourseController::class, 'deleteSoft']);
-    Route::delete('/{userCourse}/force', [UserCourseController::class, 'deleteHard']);
-
+Route::prefix('/users')->group(function () {
+    Route::get('/courses', [UserCourseController::class, 'findAll']);
+    Route::post('/{user}/courses/{course}', [UserCourseController::class, 'store']);
+    Route::patch('/{user}/courses/{course}/restore', [UserCourseController::class, 'restore']);
+    Route::delete('/{user}/courses/{course}/soft', [UserCourseController::class, 'deleteSoft']);
+    Route::delete('/{user}/courses/{course}/force', [UserCourseController::class, 'deleteHard']);
 });
 
-Route::prefix('/users/lessons')->group(function () {
-    Route::get('/', [UserLessonController::class, 'findAll']);
-    Route::get('/{userLesson}', [UserLessonController::class, 'findById']);
-    Route::post('/', [UserLessonController::class, 'store']);
-    Route::put('/{userLesson}', [UserLessonController::class, 'update']);
-    Route::patch('/{userLesson}', [UserLessonController::class, 'updatePartial']);
-    Route::patch('/restore/{userLessonId}', [UserLessonController::class, 'restore']);
-    Route::delete('/{userLesson}/soft', [UserLessonController::class, 'deleteSoft']);
-    Route::delete('/{userLesson}/force', [UserLessonController::class, 'deleteHard']);
+Route::prefix('/users')->group(function () {
+    Route::get('/lessons', [UserLessonController::class, 'findAll']);
+    Route::post('/{user}/lessons/{lessons}', [UserLessonController::class, 'store']);
+    Route::patch('/{user}/lessons/{lessons}/restore', [UserLessonController::class, 'restore']);
+    Route::delete('/{user}/lessons/{lessons}/soft', [UserLessonController::class, 'deleteSoft']);
+    Route::delete('/{user}/lessons/{lessons}/force', [UserLessonController::class, 'deleteHard']);
 
     //todo реализовать
-    Route::post('/{userLesson}/action/complete', [LessonController::class, 'askQuestion']);
-
+    Route::post('/{user}/lessons/{lessons}/action/complete', [UserLessonController::class, 'askQuestion']);
 });
